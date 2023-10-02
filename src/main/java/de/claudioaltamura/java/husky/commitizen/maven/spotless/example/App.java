73799da3780bae64eff6199172a196839cb0617f
Claudio Altamura <claudio@claudioaltamura.de>
2023-10-02 12:51:28 +0200

    chore(spotless): configured spotless

--- src/main/java/de/claudioaltamura/java/husky/commitizen/maven/spotless/example/App.java
+++ src/main/java/de/claudioaltamura/java/husky/commitizen/maven/spotless/example/App.java
@@ -4,10 +4,8 @@ package de.claudioaltamura.java.husky.commitizen.maven.spotless.example;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
